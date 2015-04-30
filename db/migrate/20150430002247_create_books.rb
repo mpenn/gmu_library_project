b9541e3d66e27f667a5fc04a35f2b803b7f2e5a2
Joe Frankiewicz <joefrank25@yahoo.com>
2015-04-29 18:35:06 -0700

    Joe

--- db/migrate/20150430002247_create_books.rb
+++ db/migrate/20150430002247_create_books.rb
@@ -1,7 +1,6 @@
-lass CreateBooks < ActiveRecord::Migration
+class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.integer :id
       t.string :isbn
       t.string :title
       t.integer :author_id
