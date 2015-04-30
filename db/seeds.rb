f05581c0985b679c20897081e22ef676089e4ca0
mpenn <matt.w.penn@gmail.com>
2015-04-29 18:23:55 -0700

    added Author and Book seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@
 tolkien = Author.where(name: 'J.R.R. Tolkien').first_or_create!(nationality: 'Canadian', dob: '07/07/1962', awards: 'Best Author Award', biography: 'This is a test bio', image_url: 'http://test.com')
 
 # Create a book
-Book.where(title: 'The Hobbit').first_or_create(isbn: '65464654', author: tolkien, author_id)
+Book.where(title: 'The Hobbit').first_or_create(isbn: '65464654', author: tolkien, genre: 'Adventure', abstract: 'This is an adventure book', pages:455, image_cover_url: 'http://test.com', published_on:'07/07/1990', total_in_library:18, created_at: 0'7/07/1991', created_at: '07/07/1993' )
