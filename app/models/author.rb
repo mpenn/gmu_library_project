7d30fb790154cec08e60bcb5fb37ccc35fd75157
mpenn <matt.w.penn@gmail.com>
2015-05-11 14:09:25 -0700

    updated seed.rb, author model, books _form.html.erb, books show.html.erb

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  #has_many :books
-  validates :name, :dob, :nationality, :biography, :image_cover_url
-  validates :biography, length: {minimum: 15}
+  has_many :books
+  validates :name, :dob, :nationality, :biography, :image_url, presence: true
+  validates :biography, length: {minimum: 18}
 end
